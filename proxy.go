bad8860690eac84effcee38b41bf4873ded3b267
Ray Ozzie <ray@ozzie.net>
2023-11-10 09:53:14 -0500

    m

--- proxy.go
+++ proxy.go
@@ -5,7 +5,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -13,26 +12,25 @@ var headerIndex = map[string][]struct{ Header, Value string }{
 
 	// Ray's dev
 	"/api.ray.blues.tools/udp": {
-		{Header: "UDP_IPV4", Value: "44.209.181.127"},
-		{Header: "UDP_PORT", Value: "8087"},
+		{Header: "udp_ipv4", Value: "44.209.181.127"},
+		{Header: "udp_port", Value: "8087"},
 	},
 
 	// Staging
 	"/api.staging.blues.tools/udp": {
-		{Header: "UDP_IPV4", Value: "44.209.181.127"},
-		{Header: "UDP_PORT", Value: "8088"},
+		{Header: "udp_ipv4", Value: "44.209.181.127"},
+		{Header: "udp_port", Value: "8088"},
 	},
 
 	// Production
 	"/api.notefile.net/udp": {
-		{Header: "UDP_IPV4", Value: "44.209.181.127"},
-		{Header: "UDP_PORT", Value: "8089"},
+		{Header: "udp_ipv4", Value: "44.209.181.127"},
+		{Header: "udp_port", Value: "8089"},
 	},
 }
 
 // Lookup the proxy for a given server
 func httpProxyLookupHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%+v\n", r)
 
 	if r.Method != "GET" || r.URL.Path == "/favicon.ico" {
 		w.WriteHeader(http.StatusNotImplemented)
@@ -40,7 +38,6 @@ func httpProxyLookupHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	headers, present := headerIndex[r.URL.Path]
-	fmt.Printf("headerIndex[%s] = %v %v", r.URL.Path, present, headers)
 	if !present {
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -48,7 +45,6 @@ func httpProxyLookupHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, v := range headers {
 		w.Header().Set(v.Header, v.Value)
-		fmt.Printf("set %s %s\n", v.Header, v.Value)
 	}
 
 	w.WriteHeader(http.StatusOK)
