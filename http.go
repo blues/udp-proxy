b5cfb235001bce1d075d61cd397397bee63f4659
Ray Ozzie <ray@ozzie.net>
2023-11-09 22:47:19 -0500

    m

--- http.go
+++ http.go
@@ -7,7 +7,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"path"
+	"time"
 )
 
 // HTTPInboundHandler kicks off inbound messages coming from all sources, then serve HTTP
@@ -21,5 +25,44 @@ func HTTPInboundHandler(port string) {
 	// HTTP
 	fmt.Printf("Now handling inbound HTTP on %s\n", port)
 	go http.ListenAndServe(port, nil)
+
+}
+
+// Ping handler
+func inboundWebPingHandler(httpRsp http.ResponseWriter, httpReq *http.Request) {
+
+	// Get the body if supplied
+	reqJSON, err := ioutil.ReadAll(httpReq.Body)
+	if err != nil {
+		reqJSON = []byte("{}")
+	}
+	_ = reqJSON
+
+	// Write reply JSON
+	rspJSON := []byte(time.Now().UTC().Format("2006-01-02T15:04:05Z"))
+	httpRsp.Write(rspJSON)
+
+}
+
+// Root handler
+func inboundWebRootHandler(httpRsp http.ResponseWriter, httpReq *http.Request) {
+
+	// Process the request URI, looking for things that will indicate "dev"
+	method := httpReq.Method
+	if method == "" {
+		method = "GET"
+	}
+
+	// Get the target
+	parsedURL, _ := url.Parse(httpReq.RequestURI)
+	target := path.Base(parsedURL.Path)
+
+	// Exit if just the favicon
+	if target == "favicon.ico" {
+		return
+	}
+
+	// Done
+	httpRsp.Write([]byte("I'm watching you."))
 
 }
