cee319c00d9aeee2c73a99810c1376e9b24d2504
Ray Ozzie <ray@ozzie.net>
2024-01-30 06:57:59 -0600

    m

--- proxy.go
+++ proxy.go
@@ -21,25 +21,25 @@ var headerIndex = map[string][]struct{ Key, Value string }{
 
 	// Scott's dev
 	"api.ray.blues.tools/udp": {
-		{Key: "udp_ipv4", Value: "44.209.181.127"},
+		{Key: "udp_ipv4", Value: "216.245.146.112"},
 		{Key: "udp_port", Value: "8086"},
 	},
 
 	// Ray's dev
 	"api.scott.blues.tools/udp": {
-		{Key: "udp_ipv4", Value: "44.209.181.127"},
+		{Key: "udp_ipv4", Value: "216.245.146.112"},
 		{Key: "udp_port", Value: "8087"},
 	},
 
 	// Staging
 	"api.staging.blues.tools/udp": {
-		{Key: "udp_ipv4", Value: "44.209.181.127"},
+		{Key: "udp_ipv4", Value: "216.245.146.112"},
 		{Key: "udp_port", Value: "8088"},
 	},
 
 	// Production
 	"api.notefile.net/udp": {
-		{Key: "udp_ipv4", Value: "44.209.181.127"},
+		{Key: "udp_ipv4", Value: "216.245.146.112"},
 		{Key: "udp_port", Value: "8089"},
 	},
 }
